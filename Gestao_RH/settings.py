150fbaaccfb742fc83f1becb300cf92d5bf57e01
PauladeCampos <pauladecamposoliveira@gmail.com>
2019-09-26 13:39:37 -0300

    Definindo relacionamento no banco de dados

--- Gestao_RH/settings.py
+++ Gestao_RH/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.empresas',
-    'apps.Funcionarios',
+    'apps.funcionarios',
     'apps.departamentos',
     'apps.documentos',
     'apps.registro_hora_extra',
--- apps/Funcionarios/admin.py
+++ apps/Funcionarios/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Funcionario
 
-# Registre seus modelos aqui.
+# Register your models here.
 admin.site.register(Funcionario)
