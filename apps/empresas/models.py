0aab09d7c709c9d495bfa49d38be8fca58715de9
PauladeCampos <pauladecamposoliveira@gmail.com>
2019-09-27 15:24:04 -0300

    Editar Empresa

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Crie seus modelo aqui.
 class Empresa(models.Model):
-    nomeFantasia = models.CharField(max_length = 100,help_text = 'Nome Fantasia da Empresa')
+    nome = models.CharField(max_length = 100,help_text = 'Nome Fantasia da Empresa')
 
     def __str__(self):
-        return self.nomeFantasia
+        return self.nome
--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
-from apps.empresas.models import Empresa
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView
+
+from .models import Empresa
 
 
 # Create your views here.
 class EmpresaCreate(CreateView):
     model = Empresa
-    fields = ['nomeFantasia']
+    fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        return HttpResponse('OK')
+
+class EmpresaEdit(UpdateView):
+    model = Empresa
+    fields = ['nome']
