e4168a59627d49f7f5181ccd65e0965aa0697e30
PauladeCampos <pauladecamposoliveira@gmail.com>
2019-09-30 16:16:42 -0300

    Crud Departamentos

---
+++ apps/departamentos/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from .views import (
+    DepartamentosList, 
+    DepartamentoNovo, 
+    DepartamentoEdit, 
+    DepartamentoDelete
+)
+
+
+urlpatterns = [
+    path('list', DepartamentosList.as_view(), name='list_departamentos'),
+    path('novo', DepartamentoNovo.as_view(), name='create_departamento'),
+    path('update/<int:pk>', DepartamentoEdit.as_view(), name='update_departamento'),
+    path('delete/<int:pk>', DepartamentoDelete.as_view(), name='delete_departamento'),
+]
